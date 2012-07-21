972b2d81839485f635335b69f3896ac2d5a884b1
Arnaud Coomans <arnaud.coomans@gmail.com>
2012-07-20 17:13:58 -0700

    Added static library and documentation targets

---
+++ AFNetworkingTests/AFNetworkingTests.m
@@ -0,0 +1,32 @@
+//
+//  AFNetworkingTests.m
+//  AFNetworkingTests
+//
+//  Created by Arnaud Coomans on 20/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AFNetworkingTests.h"
+
+@implementation AFNetworkingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AFNetworkingTests");
+}
+
+@end
